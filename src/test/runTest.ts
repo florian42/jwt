c2fca0218c0d72c09a25927e7291b4e1d5da3565
Florian <f.aumeier@me.com>
2021-02-20 18:54:54 +0100

    tests: test decodes valid input

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,23 +1,27 @@
-import * as path from 'path';
+import * as path from "path";
 
-import { runTests } from 'vscode-test';
+import { downloadAndUnzipVSCode, runTests } from "vscode-test";
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    // The path to test runner
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+    const vscodeExecutablePath = await downloadAndUnzipVSCode("1.53.1"); // Download VS Code, unzip it and run the integration test
+    await runTests({
+      extensionDevelopmentPath,
+      vscodeExecutablePath,
+      extensionTestsPath,
+    });
+  } catch (err) {
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
 }
 
 main();
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,14 +3,39 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import * as sinon from "sinon";
+import Jwt from "../../jwt";
 // import * as myExtension from '../../extension';
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
+  const validJwt =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
-  test("Should start extension @integration", async () => {
+  test("starts extension @integration", async () => {
     await vscode.commands.executeCommand("jwt.decode");
     const started = vscode.extensions.getExtension("florian.jwt")?.isActive;
     assert.strictEqual(started, true);
+  });
+
+  test("decodes valid jwt input", async () => {
+    const showInputBox = sinon.stub(vscode.window, "showInputBox");
+    const showInformationMessage = sinon.stub(
+      vscode.window,
+      "showInformationMessage"
+    );
+    showInputBox.resolves(validJwt);
+    const expectedMessage = JSON.stringify(
+      new Jwt(
+        '{"alg":"HS256","typ":"JWT"}',
+        '{"sub":"1234567890","name":"John Doe","iat":1516239022}'
+      )
+    );
+
+    await vscode.commands.executeCommand("jwt.decode");
+
+    assert(showInputBox.calledOnce);
+    assert(showInformationMessage.calledOnce);
+    assert(showInformationMessage.withArgs(expectedMessage, sinon.match.any));
   });
 });
